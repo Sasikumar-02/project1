39ab42f1a143278b78c66eb6acb84ceddb7431b2
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-03-21 00:03:40 +0530

    Firebase Social Media Project

---
+++ src/config/firebase.ts
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+import {getAuth, GoogleAuthProvider} from "firebase/auth"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDDb_kvMbcmih4xNavixhC6ieu3Ou0btDg",
+  authDomain: "project1-f982c.firebaseapp.com",
+  projectId: "project1-f982c",
+  storageBucket: "project1-f982c.appspot.com",
+  messagingSenderId: "867297556266",
+  appId: "1:867297556266:web:ba75509d3ded8b4df65053",
+  measurementId: "G-EV3NG9ZJZH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);  
+
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
