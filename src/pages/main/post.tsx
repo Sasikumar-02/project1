64725017007b3d6405aeda83974a9d247034520b
Sasi Kumar M <sasikumarmurugan02@gmail.com>
2023-03-23 11:29:03 +0530

    23-03-2023

--- src/pages/main/post.tsx
+++ src/pages/main/post.tsx
@@ -1,10 +1,32 @@
-import {Post as IPost} from "./main"
+import { addDoc, getDocs, collection, query, where } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, db } from "../../config/firebase";
+import {Post as IPost} from "./main";
 interface props {
     post: IPost;
 }
-
+interface Like {
+    userId: string;
+}
 export const Post =(props: props)=>{
     const {post} = props;
+    const [user]= useAuthState(auth);
+    const likesRef = collection(db, "likes");
+    const likesDoc = query(likesRef, where("postId", "==", post.id));
+    const [likes, setLikes] = useState<Like[] | null>(null);
+    const getLikes = async()=>{
+        const data = await getDocs(likesDoc);
+        setLikes(data.docs.map((doc)=>({userId: doc.data().userId})));
+        //setLike(data.docs.length)
+    };
+    const addLike = async() =>{
+        await addDoc(likesRef, {userID: user?.uid, postId: post.id});
+    };
+    const hasUserLiked = likes?.find
+    useEffect(()=>{
+        getLikes();
+    }, []);
     return (
         <div>
             <div>
@@ -14,9 +36,10 @@ export const Post =(props: props)=>{
                 <p>{post.description}</p>
             </div>
             <div>
-                <p>{post.username}</p>
+                <p>@{post.username}</p>
             </div>
-            <button> &#128077; </button>
+            <button onClick={addLike}> &#128077; </button>
+            {likes && <p>Likes: {likes?.length}</p>}
 
         </div>
     )
